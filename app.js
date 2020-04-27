5ea1af2ad19519ad18a94d8495eb903a9b7009de
Mateo <mgomezossa@gmail.com>
2020-04-26 20:21:54 -0500

    Mision express mongoose
    Mision Make it real

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+const app = express();
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.on("error", function(e) { console.error(e); });
+
+const UsersSchema = new mongoose.Schema({
+  name: { type: String },
+  email: { type: String },
+  password: { type: String }
+});
+const Users = mongoose.model("Users", UsersSchema);
+
+app.use(express.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+app.post("/register", async (req, res) => { //POST /register - crea al usuario en MongoDB.
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = bcrypt.hashSync(req.body.password, 10);
+  console.log(req.body);
+  const entrie = new Users({ name:name, email:email, password:password });
+  await entrie.save();
+  res.send(200);
+});
+
+app.get("/", (req, res) => { //GET / - muestra la lista de usuarios registrados.
+  const user = Users.find({}, function (err, docs) {
+    return res.send(JSON.stringify(docs));
+  });
+});
+ 
+app.listen(3000, () => console.log("Listening on port 3000 ..."));
---
+++ index.js
@@ -0,0 +1,44 @@
+    $(document).ready(function() {
+      $('.container2').hide();
+        console.log('hola');      
+    });
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        var name = $('#name').val();
+        var email = $('#email').val();
+        var password = $('#password').val();
+        console.log(name);
+        console.log(email);
+        console.log(password);
+        var tr = ('<tr>' +
+                    '<td>'+ name +'</td>'+
+                    '<td>'+ email +'</td>'+
+            '</tr>'
+                );
+      $('table').append(tr);        
+        $.ajax({
+          method: "POST",
+          url: "http://localhost:3000/register",
+          data: JSON.stringify({ name:name, email:email, password:password }),
+          contentType: "application/json"
+        }).done(function(data) {
+            alert(data); // imprimimos la respuesta
+          }).fail(function() {
+            alert("Algo salió mal");
+          }).always(function() {
+            alert("Siempre se ejecuta")
+          });
+          $('.container1').hide();
+          $('.container2').show();
+        $.getJSON("http://localhost:3000/", function (data){
+          $.each(data, function(i, item) {        
+        $("<tr>").append(            
+            $("<td>").html(item.name),
+            $("<td>").html(item.email),
+          )
+          .appendTo("table");
+            });
+          });
+           
+        });
